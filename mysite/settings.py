cc918295a95e70014927d519466f60c908267c3f
Shahvandi <90782130+Shahvandi@users.noreply.github.com>
2026-06-25 03:47:44 +0330

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,9 +14,7 @@ SECRET_KEY = 'django-insecure-_oj!$=5cwc314y=e!l4uea@+dqosyvi!@a68f-7!o3wjqole+!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "mysite-pz8f.onrender.com",
-]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
